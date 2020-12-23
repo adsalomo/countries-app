bcde3c7701266738bb6c5250dd43736646372c5f
adsalomo <adsalomo@gmail.com>
2020-12-22 19:35:37 -0500

    first commit

--- src/components/FormCountry.js
+++ src/components/FormCountry.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useForm } from '../hooks/useForm';
 
@@ -8,6 +8,8 @@ export const FormCountry = ({ setCountries }) => {
         peoples: '',
     });
 
+    const [errorMessage, setErrorMessage] = useState(null);
+
     const { country, peoples } = propertiesForm;
 
 
@@ -15,27 +17,32 @@ export const FormCountry = ({ setCountries }) => {
         e.preventDefault();
 
         if (!country || country.length <= 0 || !peoples || peoples.length <= 0) {
+            setErrorMessage('País y Habitantes es requerido');
             return;
         }
 
         console.log('Hola mundo', country, peoples);
         setCountries((countries) => [...countries, { country, peoples: Number(peoples) }]);
         handleReset();
+        setErrorMessage(null);
     }
 
 
     return (
-        <form onSubmit={(e) => handleSave(e, propertiesForm)}>
-            <div className="form-group">
-                <label>Pais</label>
-                <input onChange={handleOnChange} value={country ? country : ''} name="country" className="form-control" type="text" placeholder="Escriba pais" autoComplete="off" />
-            </div>
-            <div className="form-group">
-                <label>Habitantes</label>
-                <input onChange={handleOnChange} value={peoples ? peoples : ''} name="peoples" className="form-control" type="number" placeholder="Escriba numero" autoComplete="off" />
-            </div>
-            <button className="btn btn-primary btn-block" type="submit">Nuevo pais</button>
-        </form>
+        <>
+            <form onSubmit={(e) => handleSave(e, propertiesForm)}>
+                <div className="form-group">
+                    <label>Pais</label>
+                    <input onChange={handleOnChange} value={country ? country : ''} name="country" className="form-control" type="text" placeholder="Escriba pais" autoComplete="off" />
+                </div>
+                <div className="form-group">
+                    <label>Habitantes</label>
+                    <input onChange={handleOnChange} value={peoples ? peoples : ''} name="peoples" className="form-control" type="number" placeholder="Escriba numero" autoComplete="off" />
+                </div>
+                <button className="btn btn-primary btn-block" type="submit">Nuevo pais</button>
+                {errorMessage && <span className="text-danger">{`*${errorMessage}`}</span>}
+            </form>
+        </>
     )
 }
 
